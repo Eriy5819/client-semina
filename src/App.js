e9deb87bc4213d842cf83f5740385d28a69b00fb
Eriy Suratman <eriy5819@gmail.com>
2023-01-05 23:16:39 +0700

    ReactJS Part5

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import { useState } from 'react';
 import './App.css';
 import Button from './components/Button';
+import Input from './components/Input';
 
 function App() {
   const [number, setNumber] = useState(0);
-  const [name, setName] = useState('');
-  const [TahunLahir, setTahunLahir] = useState('');
+  // const [name, setName] = useState('');
+  // const [TahunLahir, setTahunLahir] = useState('');
+  // const [usia, setUsia] = useState('');
+  const [form, setForm] = useState({
+    name: '',
+    tahunLahir: '',
+    usia: '',
+  });
 
   const click = () => {
     setNumber(number + 1);
   };
 
-  const handleSubmit = () => {};
+  const handleSubmit = () => {
+    setForm({ ...form, usia: 2022 - form.tahunLahir });
+  };
+
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
   return (
     <>
       <h1>Counter App</h1>
@@ -20,18 +33,22 @@ function App() {
       <hr />
       <h1>Aplikasi input data diri</h1>
       Name:{' '}
-      <input
-        type="text"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
+      <Input
+        type='text'
+        value={form.name}
+        name='name'
+        onChange={handleChange}
       />
       <br />
       Tahun lahir:{' '}
-      <input
-        type="text"
-        value={TahunLahir}
-        onChange={(e) => TahunLahir(e.target.value)}
+      <Input
+        type='number'
+        value={form.tahunLahir}
+        name='tahunLahir'
+        onChange={handleChange}
       />
+      <br />
+      Umur saya : {form.usia}
       <br />
       <Button onClick={handleSubmit}>Submit</Button>
     </>
